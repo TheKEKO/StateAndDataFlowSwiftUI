ed8e8241ea021a8c6c8f0d30de2024f4741c3ea2
Aleksandr F <a7214370@gmail.com>
2022-05-19 19:58:08 +0300

    Initial Commit

---
+++ StateAndDataFlowSwiftUI/StateAndDataFlowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowSwiftUIApp.swift
+//  StateAndDataFlowSwiftUI
+//
+//  Created by Aleksandr F. on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
