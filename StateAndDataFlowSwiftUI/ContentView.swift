74dd00595ef3a14cb85d529c990c8ce2bb1afbbe
Aleksandr F <a7214370@gmail.com>
2022-05-21 02:12:02 +0300

    Setup ContentView

--- StateAndDataFlowSwiftUI/ContentView.swift
+++ StateAndDataFlowSwiftUI/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject private var user: UserManager
+    @StateObject private var timer = TimeCounter()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hi, \(user.name)")
+                .font(.largeTitle)
+                .padding(.top, 80)
+            Text("\(timer.counter)")
+                .font(.largeTitle)
+                .padding(.top, 80)
+            Spacer()
+            ButtonTimerView(timer: timer)
+            ButtonView(action: {user.name = ""},
+                       titleOfButton: "LogOut",
+                       color: .blue)
+            .padding(.top, 200)
+        }
+        .padding()
+    }
+}
+
+struct ButtonTimerView: View {
+    @ObservedObject var timer: TimeCounter
+    
+    var body: some View {
+        ButtonView(action: timer.startTimer, titleOfButton: timer.buttonTitle, color: .red)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(UserManager())
     }
 }
+
+
