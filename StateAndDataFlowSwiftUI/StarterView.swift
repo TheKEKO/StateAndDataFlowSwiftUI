3734ee57ebfd697d528f22998c08852856cc6def
Aleksandr F <a7214370@gmail.com>
2022-05-21 02:12:45 +0300

    Setup StarterView

--- StateAndDataFlowSwiftUI/StarterView.swift
+++ StateAndDataFlowSwiftUI/StarterView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct StarterView: View {
+    @EnvironmentObject private var userManager: UserManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            if !userManager.name.isEmpty {
+                ContentView()
+            } else {
+                RegisterView()
+            }
+        }
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+struct StarterView_Previews: PreviewProvider {
     static var previews: some View {
         StarterView()
+            .environmentObject(UserManager())
     }
 }
