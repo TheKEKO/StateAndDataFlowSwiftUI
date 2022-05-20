03583f2dfeca5177cba1e82bb5418a4ba3911970
Aleksandr F <a7214370@gmail.com>
2022-05-21 02:12:26 +0300

    Setup RegisterView

--- StateAndDataFlowSwiftUI/RegisterView.swift
+++ StateAndDataFlowSwiftUI/RegisterView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @EnvironmentObject private var userManger: UserManager
+    @State private var name = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                TextField("Enter your name...", text: $name)
+                    .multilineTextAlignment(.center)
+                Text("\(name.count)")
+                    .foregroundColor(name.count < 3 ? .red : .green)
+                    .padding(.trailing, 20)
+            }
+            Button(action: registerUser) {
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                    Text("OK")
+                }
+            }
+            .disabled(name.count < 3)
+        }
+    }
+    
+    private func registerUser() {
+        if !name.isEmpty {
+            userManger.name = name
+            userManger.isRegistered.toggle()
+        }
     }
 }
 
