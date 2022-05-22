f332a17c5ca625bceee6614a466097735f8729a9
Aleksandr F <a7214370@gmail.com>
2022-05-22 23:27:29 +0300

    Setup DataManager

---
+++ StateAndDataFlowSwiftUI/DataManager.swift
@@ -0,0 +1,20 @@
+//
+//  DataManager.swift
+//  StateAndDataFlowSwiftUI
+//
+//  Created by Aleksandr F. on 22.05.2022.
+//
+
+import SwiftUI
+
+struct DataManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DataManager_Previews: PreviewProvider {
+    static var previews: some View {
+        DataManager()
+    }
+}
