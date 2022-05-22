ffc673b3131766b92a130372498309bdc3001dc9
Aleksandr F <a7214370@gmail.com>
2022-05-22 23:26:56 +0300

    Setup ButtonAppearanceModifier

---
+++ StateAndDataFlowSwiftUI/ButtonAppearanceModifier.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonAppearanceModifier.swift
+//  StateAndDataFlowSwiftUI
+//
+//  Created by Aleksandr F. on 22.05.2022.
+//
+
+import SwiftUI
+
+struct ButtonAppearanceModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonAppearanceModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonAppearanceModifier()
+    }
+}
